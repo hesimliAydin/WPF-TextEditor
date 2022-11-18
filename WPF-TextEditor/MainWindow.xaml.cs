1767acff013ed8dbc6fd0d8a04e4811dca254821
Aydin Hesimli <aydinhsimli@gmail.com>
2022-11-18 19:48:11 +0400

    Add project files.

---
+++ WPF-TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFileDialogClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSaveDialogClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkAutoSaveChecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cpTextColorSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+
+        }
+
+        private void cpBackColorSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+
+        }
+    }
+}

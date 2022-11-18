232f184619200d3c5a2688a7546bb7799cb8359a
Aydin Hesimli <aydinhsimli@gmail.com>
2022-11-18 21:35:52 +0400

    add

--- WPF-TextEditor/MainWindow.xaml.cs
+++ WPF-TextEditor/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,9 @@ namespace WPF_TextEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool _isAutoSaveUsed = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,27 +32,92 @@ namespace WPF_TextEditor
 
         private void btnFileDialogClick(object sender, RoutedEventArgs e)
         {
+            var fileDialog = new OpenFileDialog()
+            {
+                Filter = "Text|*.txt"
+            };
 
+            if (fileDialog.ShowDialog() is true)
+            {
+                using StreamReader streamReader = new(fileDialog.FileName);
+                txt.Selection.Text = streamReader.ReadToEnd();
+            }
+            
         }
 
         private void btnSaveDialogClick(object sender, RoutedEventArgs e)
         {
+            var saveFile = new SaveFileDialog();
 
+
+
+            if (saveFile.ShowDialog() is true)
+            {
+                txt.SelectAll();
+                using StreamWriter streamWriter = new(saveFile.FileName);
+                streamWriter.Write(txt.Selection);
+            }
         }
 
         private void chkAutoSaveChecked(object sender, RoutedEventArgs e)
         {
+            if (_isAutoSaveUsed == false)
+            {
 
+                var result = MessageBox.Show("Tour Text Will be automatically saved on Desktop with Name AydinText.txt \nDo you accept It?", "Information", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    _isAutoSaveUsed = true;
+                    return;
+                }
+
+                chkAutoSave.IsChecked = false;
+            }
         }
 
         private void cpTextColorSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
+            if (cpTextColor.SelectedColor is System.Windows.Media.Color color)
+            {
+                if (txt.Selection.IsEmpty)
+                    txt.Foreground = new SolidColorBrush(cpTextColor.SelectedColor.Value);
+                else
+                    txt.Selection.ApplyPropertyValue(ForegroundProperty, new System.Windows.Media.SolidColorBrush(cpTextColor.SelectedColor.Value));
 
+            }
         }
 
         private void cpBackColorSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
+            if (cpBackColor.SelectedColor is System.Windows.Media.Color color)
+                txt.Background = new SolidColorBrush(color);
+        }
 
+        private void txtTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (chkAutoSave.IsChecked == null || chkAutoSave.IsChecked == false)
+                return;
+
+            txt.SelectAll();
+
+            File.WriteAllText(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\AydinText.txt", txt.Selection.Text);
+        }
+
+        private void cBoxFontStyleSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (txt.Selection.IsEmpty)
+                txt.FontFamily = new System.Windows.Media.FontFamily(cBoxFontStyle.SelectedItem.ToString());
+            else
+                txt.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, new System.Windows.Media.FontFamily(cBoxFontStyle.SelectedItem.ToString()));
+        }
+
+        private void cBoxFontSizeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (txt.Selection.IsEmpty)
+                txt.FontSize = double.Parse(cBoxFontSize.SelectedItem.ToString()!);
+            else
+                txt.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, cBoxFontSize.SelectedItem.ToString());
         }
     }
 }
